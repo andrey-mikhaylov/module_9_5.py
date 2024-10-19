d832611e1ab1d22366e660454512859adf723576
Andrey <andrey.mikhaylov.n@gmail.com>
2024-10-19 10:05:30 +0300

    done

--- module_9_5.py
+++ module_9_5.py
@@ -1,3 +1,85 @@
+from math import copysign
+from typing import Iterator
+
+
+class StepValueError(ValueError):
+    pass
+
+
+class Iterator:
+    def __init__(self, start: int, stop: int, step: int = 1):
+        """
+        :param start: целое число с которого начинается итерация.
+        :param stop: целое число на котором заканчивается итерация.
+        :param step: шаг с которой совершается итерация.
+        """
+        if step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.pointer = start # указывает на текущее число в итерации (изначально start)
+
+    def __iter__(self):
+        """
+        Сбрасывает значение pointer на start
+        :return: объект итератора.
+        """
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        value = self.pointer
+        if self.step > 0 and value > self.stop or self.step < 0 and value < self.stop:
+            raise StopIteration
+
+        self.pointer += self.step
+        return value
+
+
+def test():
+    try:
+        iter1 = Iterator(100, 200, 0)
+        for i in iter1:
+            print(i, end=' ')
+    except StepValueError:
+        print('Шаг указан неверно')
+
+    iter2 = Iterator(-5, 1)
+    iter3 = Iterator(6, 15, 2)
+    iter4 = Iterator(5, 1, -1)
+    iter5 = Iterator(10, 1)
+
+    for i in iter2:
+        print(i, end=' ')
+    print()
+
+    for i in iter3:
+        print(i, end=' ')
+    print()
+
+    for i in iter4:
+        print(i, end=' ')
+    print()
+
+    for i in iter5:
+        print(i, end=' ')
+    print()
+
+    """
+Вывод на консоль:
+Шаг указан неверно
+-5 -4 -3 -2 -1 0 1
+6 8 10 12 14
+5 4 3 2 1
+    """
+
+
+if __name__ == '__main__':
+    test()
+
+
 """
 2023/12/03 00:00|Домашнее задание по теме "Итераторы"
 Если вы решали старую версию задачи, проверка будет производиться по ней.
